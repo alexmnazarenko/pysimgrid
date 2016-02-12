f01e0d668d560e5c36317dbd3b3af0dd19c656f2
Alexey Nazarenko <alexmnazarenko@gmail.com>
2016-02-12 17:52:10 +0300

    Working list scheduling heuristic

--- Runner/src/simulator.cpp
+++ Runner/src/simulator.cpp
@@ -1,8 +1,7 @@
 // (c) DATADVANCE 2016
 
 #include "simulator.hpp"
-#include "simgrid/platf.h"
-#include "simgrid/simdag.h"
+#include "scheduler.hpp"
 
 #include <rapidjson/document.h>
 #include <boost/filesystem.hpp>
@@ -33,23 +32,117 @@ namespace bfs = boost::filesystem;
 
 namespace darunner {
 
-Simulator::Simulator(const std::string& platform_path, const std::string& tasks_path) {
+const std::string Simulator::ROOT_TASK = "root";
+const std::string Simulator::END_TASK = "end";
+
+
+Simulator::Simulator(const std::string& platform_path, const std::string& tasks_path, TaskFormat task_format) {
   if (!bfs::is_regular_file(platform_path)) {
     throw std::runtime_error("platform configuration file does not exist");
   }
   if (!bfs::is_regular_file(tasks_path)) {
     throw std::runtime_error("platform configuration file does not exist");
   }
+  // Load platform info and store it for convenience
   XBT_INFO("Loading platform from '%s'", platform_path.c_str());
   SD_create_environment(platform_path.c_str());
-  _load_tasks_json(tasks_path);
+  _workstations.assign(SD_workstation_get_list(), SD_workstation_get_list() + SD_workstation_get_number());
+  _links.assign(SD_link_get_list(), SD_link_get_list() + SD_link_get_number());
+  // Attach data to workstations
+  for (auto& workstation: _workstations) {
+    SD_workstation_set_data(workstation, new WorkstationData);
+  }
+
+  switch (task_format) {
+  case TASK_FORMAT_DOT:
+    _load_tasks_dot(tasks_path);
+    break;
+  case TASK_FORMAT_JSON:
+    throw std::runtime_error("json task format not implemented yet");
+    _load_tasks_json(tasks_path);
+    break;
+  default:
+    throw std::runtime_error("unknown task format");
+  }
+
+  for (auto& task: _tasks) {
+    SD_task_set_data(task, new TaskData);
+  }
 }
 
-Simulator::~Simulator() noexcept {}
 
-void Simulator::simulate(Scheduler& scheduler) {
-
+Simulator::~Simulator() noexcept {
+  for (auto& task: _tasks) {
+    TaskData* const data = reinterpret_cast<TaskData*>(SD_task_get_data(task));
+    delete data;
+    SD_task_destroy(task);
+  }
+  for (auto& workstation: _workstations) {
+    WorkstationData* const data = reinterpret_cast<WorkstationData*>(SD_workstation_get_data(workstation));
+    delete data;
+  }
 }
+
+
+Simulator::WorkstationData* Simulator::workstation_get_data(SD_workstation_t const workstation) {
+  void* const data = SD_workstation_get_data(workstation);
+  BOOST_ASSERT(data && "no attached data on workstation");
+  return reinterpret_cast<WorkstationData*>(data);
+}
+
+
+Simulator::TaskData* Simulator::task_get_data(SD_task_t const task) {
+  void* const data = SD_task_get_data(task);
+  BOOST_ASSERT(data && "no attached data on task");
+  return reinterpret_cast<TaskData*>(data);
+}
+
+
+SD_task_t Simulator::task_by_name(const std::string& name) {
+  for (auto& task: _tasks) {
+    if (SD_task_get_name(task) == name) {
+      return task;
+    }
+  }
+  throw std::runtime_error("task with a given name does not exist");
+}
+
+
+void Simulator::simulate(Scheduler::Algorithm schedule) {
+  std::unique_ptr<Scheduler> scheduler = Scheduler::create(schedule, *this);
+  switch (scheduler->type()) {
+  case Scheduler::Type::STATIC:
+    scheduler->schedule();
+    SD_simulate(-1);
+    break;
+  case Scheduler::Type::DYNAMIC:
+    for (auto task: _tasks) {
+      if (SD_task_get_kind(task) == SD_TASK_COMP_SEQ) {
+        SD_task_watch(task, SD_DONE);
+      }
+    }
+    scheduler->schedule();
+    while (!xbt_dynar_is_empty(SD_simulate(-1))) {
+      scheduler->schedule();
+    }
+    break;
+  default:
+    throw std::runtime_error("unknown scheduler type");
+  }
+  XBT_INFO("Simulation time: %f seconds\n", SD_get_clock());
+}
+
+
+void Simulator::_load_tasks_dot(const std::string& tasks_path) {
+  xbt_dynar_t task_array = SD_dotload(tasks_path.c_str());
+  unsigned idx;
+  SD_task_t element;
+  xbt_dynar_foreach(task_array, idx, element) {
+    _tasks.push_back(element);
+  }
+  xbt_dynar_free_container(&task_array);
+}
+
 
 void Simulator::_load_tasks_json(const std::string& tasks_path) {
   // TODO: finalize when extended load will be required
--- Runner/src/simulator.hpp
+++ Runner/src/simulator.hpp
@@ -1,27 +1,65 @@
 // (c) DATADVANCE 2016
 
+#pragma once
+
+#include "scheduler.hpp"
+
+#include "simgrid/platf.h"
+#include "simgrid/simdag.h"
+
+#include <vector>
 #include <string>
 #include <memory>
 
 namespace darunner {
 
-class Scheduler;
-
 class Simulator {
 public:
-  Simulator(const std::string& platform_path, const std::string& tasks_path);
+  enum TaskFormat {
+    TASK_FORMAT_DOT,
+    TASK_FORMAT_JSON
+  };
+
+  struct TaskData {
+    SD_workstation_t executor_guess = nullptr;
+    double completion_guess = 0;
+  };
+
+  struct WorkstationData {
+    bool is_submission_node = false;
+    double available_at = 0.;
+  };
+
+  static const std::string ROOT_TASK;
+  static const std::string END_TASK;
+
+  Simulator(const std::string& platform_path, const std::string& tasks_path, TaskFormat task_format = TASK_FORMAT_DOT);
   ~Simulator() noexcept;
+
+  std::vector<SD_workstation_t>& get_workstations() { return _workstations; }
+  std::vector<SD_link_t>& get_links() { return _links; }
+  std::vector<SD_task_t>& get_tasks() { return _tasks; }
+
+  WorkstationData* workstation_get_data(SD_workstation_t const workstation);
+  TaskData* task_get_data(SD_task_t const task);
+
+  SD_task_t task_by_name(const std::string& name);
 
   /**
    * Run SimGrid simulation, probably in multiple steps if dynamic scheduling is used.
    */
-  void simulate(Scheduler& scheduler);
+  void simulate(Scheduler::Algorithm schedule);
 
 private:
   // Shouldn't be ever copied
   Simulator(const Simulator&);
 
+  void _load_tasks_dot(const std::string& tasks_path);
   void _load_tasks_json(const std::string& tasks_path);
+
+  std::vector<SD_workstation_t> _workstations;
+  std::vector<SD_link_t> _links;
+  std::vector<SD_task_t> _tasks;
 };
 
 }
