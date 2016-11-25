0051cfdb96ff6587cbf3f17e95a319eb6a10b98e
andrey.voronov <andrey.voronov@datadvance.net>
2016-11-25 18:32:45 +0300

    Add Lookahead algorithm, rewrite HEFT and taskflow for Lookahead fractional use

--- pysimgrid/simdag/algorithms/heft.py
+++ pysimgrid/simdag/algorithms/heft.py
@@ -14,7 +14,23 @@ from ..taskflow import Taskflow
 
 class HEFTScheduler(StaticScheduler):
 
-  def _rankify_tasks(self, taskflow):
+  def get_schedule(self, simulation):
+    taskflow = Taskflow().from_simulation(simulation)
+    tasks_map = {t.name: t for t in simulation.tasks}
+    raw_schedule = self.construct_heft_schedule(taskflow, simulation.hosts)
+
+    schedule = {}
+    for host in simulation.hosts:
+      schedule[host] = []
+      for elem in raw_schedule[host.name]["timesheet"]:
+        task_name = elem[2][0]
+        if task_name not in [taskflow.TRUE_ROOT, taskflow.TRUE_END]:
+          schedule[host].append(tasks_map[task_name])
+
+    return schedule
+
+  @staticmethod
+  def rankify_tasks(taskflow):
     ranked_tasks = {}
     for task in taskflow.topological_order(reverse=True):
       ecomt_and_rank = [
@@ -26,55 +42,79 @@ class HEFTScheduler(StaticScheduler):
       ranked_tasks[task] = taskflow.complexities[task] + max(ecomt_and_rank)
     return ranked_tasks
 
-  def _order_tasks(self, ranked_tasks):
+  @staticmethod
+  def order_tasks(ranked_tasks):
     return sorted(ranked_tasks.items(), key=lambda x: x[1], reverse=True)
 
-  def _timesheet_gaps(self, timesheet):
-    pairs = zip(timesheet, timesheet[1:])
-    return [(p[0][1], p[1][0]) for p in pairs if p[0][1] != p[1][0]]
+  @staticmethod
+  def construct_heft_schedule(taskflow, simulation_hosts, initial_schedule=None, initial_taskname=None):
 
-  def _calc_host_start(self, est, amount, hosts):
-    e_host_st = []
-    for host in hosts:
-      # Check host gaps
-      duration = float(amount) / hosts[host]["speed"]
-      gaps = self._timesheet_gaps(hosts[host]["timesheet"])
-      for gap in gaps:
-        start = max(gap[0], est)
-        end = gap[1]
-        if end < est or duration > end - start:
-          continue
-        e_host_st.append((host, start, start + duration))
-        break
-      if host not in e_host_st:
-        # End time of the last host task
-        start = (
-          hosts[host]["timesheet"][-1][1]
-          if len(hosts[host]["timesheet"])
-          else 0
-        )
-        start = max(start, est)
-        e_host_st.append((host, start, start + duration))
-    return min(e_host_st, key=lambda x: x[2])
+    def _timesheet_gaps(timesheet):
+      pairs = zip(timesheet, timesheet[1:])
+      return [(p[0][1], p[1][0]) for p in pairs if p[0][1] != p[1][0]]
 
-  def get_schedule(self, simulation):
-    taskflow = Taskflow(simulation.tasks)
-    tasks_map = {t.name: t for t in simulation.tasks}
-    ranked_tasks = self._rankify_tasks(taskflow)
-    ordered_tasks = self._order_tasks(ranked_tasks)
+    def _calc_host_start(est, amount, hosts):
+      e_host_st = []
+      for host in hosts:
+        # Check host gaps
+        duration = float(amount) / hosts[host]["speed"]
+        gaps = _timesheet_gaps(hosts[host]["timesheet"])
+        for gap in gaps:
+          start = max(gap[0], est)
+          end = gap[1]
+          if end < est or duration > end - start:
+            continue
+          e_host_st.append((host, start, start + duration))
+          break
+        if host not in e_host_st:
+          # End time of the last host task
+          start = (
+            hosts[host]["timesheet"][-1][1]
+            if len(hosts[host]["timesheet"])
+            else 0
+          )
+          start = max(start, est)
+          e_host_st.append((host, start, start + duration))
+      return min(e_host_st, key=lambda x: x[2])
 
-    hosts = {
-      host.name: {
-        "speed": host.speed,
-        "timesheet": []
-      }
-      for host in simulation.hosts
-    }
+    ranked_tasks = HEFTScheduler.rankify_tasks(taskflow)
+    ordered_tasks = HEFTScheduler.order_tasks(ranked_tasks)
+
+    initial_start_time = 0
     tasks_info = {}
+    if not initial_schedule:
+      hosts = {
+        host.name: {
+          "speed": host.speed,
+          "timesheet": []
+        }
+        for host in simulation_hosts
+      }
+    else:
+      hosts = deepcopy(initial_schedule)
+
+      for host in hosts:
+        for task_info in hosts[host]['timesheet']:
+          start, end, task_element = task_info
+          task_name, _ = task_element
+          if task_name == initial_taskname:
+            initial_start_time = end
+          tasks_info[task_name] = {
+            "host": host,
+            "start_time": start,
+            "end_time": end
+          }
+
     for task in ordered_tasks:
       parents = taskflow.get_parents(task[0])
-      est = max([tasks_info[p]["end_time"] for p in parents if p in tasks_info] + [0])
-      host, start_time, end_time = self._calc_host_start(
+      if initial_taskname:
+        parents.append(initial_taskname)
+      est = max(
+        [tasks_info[p]["end_time"] for p in parents if p in tasks_info] +
+        [0] + 
+        [initial_start_time]
+      )
+      host, start_time, end_time = _calc_host_start(
         est,
         taskflow.complexities[task[0]],
         hosts
@@ -87,10 +127,4 @@ class HEFTScheduler(StaticScheduler):
       hosts[host]["timesheet"].append((start_time, end_time, task))
       hosts[host]["timesheet"].sort()
 
-    schedule = {}
-    for host in simulation.hosts:
-      schedule[host] = []
-      for elem in hosts[host.name]["timesheet"]:
-        task_name = elem[2][0]
-        schedule[host].append(tasks_map[task_name])
-    return schedule
+    return hosts
