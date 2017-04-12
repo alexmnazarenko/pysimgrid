244938fb02a89dfb078d2f6f283e746603886d73
Alexey Nazarenko <alexmnazarenko@gmail.com>
2017-04-12 18:59:02 +0300

    updated DAG scripts

--- dag/prepare.sh
+++ dag/prepare.sh
@@ -11,7 +11,7 @@ if [ ! -f "$DAGGEN_PATH" ]; then
 fi
 
 #
-# Experiment 1/2:
+# Experiment 1:
 # Heterogeneous clusters with homogeneous network a-la 1Gbit Ethernet.
 #
 # Speed:     1-4 GFLOPS
@@ -19,9 +19,38 @@ fi
 # Latency:   100 us
 #
 if [ ! -d "$SCRIPT_DIR/plat_exp1" ]; then
-  python3 -m pysimgrid.tools.plat_gen "$SCRIPT_DIR/plat_exp1" 100 cluster 5 1-4 100 100
-  python3 -m pysimgrid.tools.plat_gen "$SCRIPT_DIR/plat_exp1" 100 cluster 10 1-4 100 100
-  python3 -m pysimgrid.tools.plat_gen "$SCRIPT_DIR/plat_exp1" 100 cluster 20 1-4 100 100
+  python3 -m pysimgrid.tools.plat_gen "$SCRIPT_DIR/plat_exp1" 100 cluster 5 1-4 100 100 --include_master
+  python3 -m pysimgrid.tools.plat_gen "$SCRIPT_DIR/plat_exp1" 100 cluster 10 1-4 100 100 --include_master
+  python3 -m pysimgrid.tools.plat_gen "$SCRIPT_DIR/plat_exp1" 100 cluster 20 1-4 100 100 --include_master
+fi
+
+#
+# Experiment 1_inf:
+# Heterogeneous clusters with homogeneous network a-la 1Gbit Ethernet.
+#
+# Speed:     1-4 GFLOPS
+# Bandwidth: 1e12 MBps
+# Latency:   0
+#
+if [ ! -d "$SCRIPT_DIR/plat_exp1_inf" ]; then
+  python3 -m pysimgrid.tools.plat_gen "$SCRIPT_DIR/plat_exp1_inf" 100 cluster 5 1-4 1e12 0 --include_master
+  python3 -m pysimgrid.tools.plat_gen "$SCRIPT_DIR/plat_exp1_inf" 100 cluster 10 1-4 1e12 0 --include_master
+  python3 -m pysimgrid.tools.plat_gen "$SCRIPT_DIR/plat_exp1_inf" 100 cluster 20 1-4 1e12 0 --include_master
+fi
+
+
+if [ ! -d "$SCRIPT_DIR/plat_exp2" ]; then
+  python3 -m pysimgrid.tools.plat_gen "$SCRIPT_DIR/plat_exp2" 1 cluster 10 1-4 100 100 --include_master
+fi
+
+
+if [ ! -d "$SCRIPT_DIR/plat_exp2_inf" ]; then
+  python3 -m pysimgrid.tools.plat_gen "$SCRIPT_DIR/plat_exp2_inf" 1 cluster 10 1-4 1e12 0 --loopback_bandwidth=1e12 --loopback_latency=0 --include_master
+fi
+
+
+if [ ! -d "$SCRIPT_DIR/tasks_exp2" ]; then
+  python3 -m pysimgrid.tools.dag_gen "$DAGGEN_PATH" "$SCRIPT_DIR/tasks_exp2" --count 100 --fat 0.5 0.65 0.8 --density 0.1 --jump 2 --force_ccr 1 10 100 500 1000 --repeat 100
 fi
 
 #
@@ -33,19 +62,20 @@ fi
 # Latency:   100 us
 #
 if [ ! -d "$SCRIPT_DIR/plat_exp3" ]; then
-  python3 -m pysimgrid.tools.plat_gen "$SCRIPT_DIR/plat_exp3" 1 cluster 5 1-4 100 100
-  python3 -m pysimgrid.tools.plat_gen "$SCRIPT_DIR/plat_exp3" 1 cluster 10 1-4 100 100
-  python3 -m pysimgrid.tools.plat_gen "$SCRIPT_DIR/plat_exp3" 1 cluster 20 1-4 100 100
-  python3 -m pysimgrid.tools.plat_gen "$SCRIPT_DIR/plat_exp3" 1 cluster 5 1-4 20 100
-  python3 -m pysimgrid.tools.plat_gen "$SCRIPT_DIR/plat_exp3" 1 cluster 10 1-4 20 100
-  python3 -m pysimgrid.tools.plat_gen "$SCRIPT_DIR/plat_exp3" 1 cluster 20 1-4 20 100
-  python3 -m pysimgrid.tools.plat_gen "$SCRIPT_DIR/plat_exp3" 1 cluster 5 1-4 10 100
-  python3 -m pysimgrid.tools.plat_gen "$SCRIPT_DIR/plat_exp3" 1 cluster 10 1-4 10 100
-  python3 -m pysimgrid.tools.plat_gen "$SCRIPT_DIR/plat_exp3" 1 cluster 20 1-4 10 100
+  python3 -m pysimgrid.tools.plat_gen "$SCRIPT_DIR/plat_exp3" 1 cluster 5 1-4 100 100 --include_master
+  python3 -m pysimgrid.tools.plat_gen "$SCRIPT_DIR/plat_exp3" 1 cluster 10 1-4 100 100 --include_master
+  python3 -m pysimgrid.tools.plat_gen "$SCRIPT_DIR/plat_exp3" 1 cluster 20 1-4 100 100 --include_master
+  python3 -m pysimgrid.tools.plat_gen "$SCRIPT_DIR/plat_exp3" 1 cluster 5 1-4 20 100 --include_master
+  python3 -m pysimgrid.tools.plat_gen "$SCRIPT_DIR/plat_exp3" 1 cluster 10 1-4 20 100 --include_master
+  python3 -m pysimgrid.tools.plat_gen "$SCRIPT_DIR/plat_exp3" 1 cluster 20 1-4 20 100 --include_master
+  python3 -m pysimgrid.tools.plat_gen "$SCRIPT_DIR/plat_exp3" 1 cluster 5 1-4 10 100 --include_master
+  python3 -m pysimgrid.tools.plat_gen "$SCRIPT_DIR/plat_exp3" 1 cluster 10 1-4 10 100 --include_master
+  python3 -m pysimgrid.tools.plat_gen "$SCRIPT_DIR/plat_exp3" 1 cluster 20 1-4 10 100 --include_master
 fi
 
 if [ ! -d "$SCRIPT_DIR/tasks_exp3" ]; then
-  python3 -m pysimgrid.tools.dag_gen "$DAGGEN_PATH" "$SCRIPT_DIR/tasks_exp3" --count 20 40 60 80 100 --fat 0.2 0.4 0.6 --density 0.1 0.2 0.3 0.4 --jump 1 2 3 --regular 0.1 0.5 0.9 --repeat 5
+  python3 -m pysimgrid.tools.dag_gen "$DAGGEN_PATH" "$SCRIPT_DIR/tasks_exp3" --count 20 40 60 80 100 --fat 0.1 0.2 0.4 --density 0.1 0.2 0.3 0.4 --jump 1 2 3 --regular 0.1 0.5 0.9 --repeat 5
 fi
+
 
 popd
