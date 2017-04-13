07b7895ac73a67618e9c50ca38e2356c4244ffcc
Oleg Sukhoroslov <oleg.sukhoroslov@gmail.com>
2017-04-13 13:38:00 +0300

    Fixed results path

--- dag/run.sh
+++ dag/run.sh
@@ -5,19 +5,19 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 pushd "$SCRIPT_DIR/.."
 
 if [ ! -f "$SCRIPT_DIR/exp1.json" ]; then
-  python3 -m pysimgrid.tools.experiment "$SCRIPT_DIR/plat_exp1" "$SCRIPT_DIR/tasks_exp1" "$SCRIPT_DIR/algorithms.json" dag2/exp1.json -j8 --simgrid-log-level=error
+  python3 -m pysimgrid.tools.experiment "$SCRIPT_DIR/plat_exp1" "$SCRIPT_DIR/tasks_exp1" "$SCRIPT_DIR/algorithms.json" dag/exp1.json -j8 --simgrid-log-level=error
 fi
 
 if [ ! -f "$SCRIPT_DIR/exp1_inf.json" ]; then
-  python3 -m pysimgrid.tools.experiment "$SCRIPT_DIR/plat_exp1_inf" "$SCRIPT_DIR/tasks_exp1" "$SCRIPT_DIR/algorithms.json" dag2/exp1_inf.json -j8 --simgrid-log-level=error
+  python3 -m pysimgrid.tools.experiment "$SCRIPT_DIR/plat_exp1_inf" "$SCRIPT_DIR/tasks_exp1" "$SCRIPT_DIR/algorithms.json" dag/exp1_inf.json -j8 --simgrid-log-level=error
 fi
 
 if [ ! -f "$SCRIPT_DIR/exp2.json" ]; then
-  python3 -m pysimgrid.tools.experiment "$SCRIPT_DIR/plat_exp2" "$SCRIPT_DIR/tasks_exp2" "$SCRIPT_DIR/algorithms.json" dag2/exp2.json -j8
+  python3 -m pysimgrid.tools.experiment "$SCRIPT_DIR/plat_exp2" "$SCRIPT_DIR/tasks_exp2" "$SCRIPT_DIR/algorithms.json" dag/exp2.json -j8
 fi
 
 if [ ! -f "$SCRIPT_DIR/exp2_inf.json" ]; then
-  python3 -m pysimgrid.tools.experiment "$SCRIPT_DIR/plat_exp2_inf" "$SCRIPT_DIR/tasks_exp2" "$SCRIPT_DIR/algorithms.json" dag2/exp2_inf.json -j8
+  python3 -m pysimgrid.tools.experiment "$SCRIPT_DIR/plat_exp2_inf" "$SCRIPT_DIR/tasks_exp2" "$SCRIPT_DIR/algorithms.json" dag/exp2_inf.json -j8
 fi
 
 popd
