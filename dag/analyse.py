e23d83f18ae41cb32e25f505e553272ed4c9b3ee
Oleg Sukhoroslov <oleg.sukhoroslov@gmail.com>
2018-12-09 18:20:57 +0300

    Improved workflow experiments example

--- dag/analyse.py
+++ dag/analyse.py
@@ -9,126 +9,169 @@ from __future__ import print_function
 import argparse
 import collections
 import json
+import numpy
 import os
 import textwrap
 
-import numpy
-
 
 def groupby(results, condition, asitems=True):
-  groups = collections.defaultdict(list)
-  for data in results:
-    key = condition(data)
-    groups[key].append(data)
-  return groups.items() if asitems else groups
+    groups = collections.defaultdict(list)
+    for data in results:
+        key = condition(data)
+        groups[key].append(data)
+    return groups.items() if asitems else groups
 
 
-def par(string):
-  return textwrap.dedent(string).strip()
+def get_app_filename(item):
+    return str(os.path.basename(item["tasks"]).rsplit(".", 1)[0])
 
 
-def get_taskfile_name(item):
-  return os.path.basename(item["tasks"]).rsplit(".", 1)[0]
+def get_app_group(item):
+    return "_".join(get_app_filename(item).split("_")[:2])
 
 
-def get_task_count(item):
-  return int(get_taskfile_name(item).split("_")[1])
+def get_fat(item):
+    return float(get_app_filename(item).split("_")[2])
 
 
-def get_taskfile_group(item):
-  return "_".join(get_taskfile_name(item).split("_")[:2])
+def get_ccr(item):
+    return float(get_app_filename(item).split("_")[8])
 
 
 def get_platform_name(item):
-  return os.path.basename(item["platform"])
+    return os.path.basename(item["platform"])
 
 
 def get_host_count(item):
-  return int(get_platform_name(item).split("_")[1])
-
-
-def get_host_bandwidth(item):
-  return int(get_platform_name(item).split("_")[3])
+    return int(get_platform_name(item).split("_")[1])
 
 
 def get_algorithm(item):
-  return item["algorithm"]["name"]
+    return item["algorithm"]["name"]
 
 
 def main():
-  MODES = {
-    "tgroup_hcount": lambda results: normtime_all_algo(results, get_taskfile_group, get_host_count),
-    "tgroup_hcount_exp": lambda results: etime_static_algo(results, get_taskfile_group, get_host_count),
-    "bandwidth_hcount": lambda results: normtime_all_algo(results, get_host_bandwidth, get_host_count),
-    "bandwidth_hcount_exp": lambda results: etime_static_algo(results, get_host_bandwidth, get_host_count)
-  }
+    algorithms = ["OLB", "MCT", "MinMin", "MaxMin", "Sufferage", "DLS", "HEFT", "HCPT", "Lookahead", "PEFT"]
 
-  parser = argparse.ArgumentParser(description="Experiment results analysis")
-  parser.add_argument("input_file", type=str, help="experiment results")
-  parser.add_argument("-m", "--mode", type=str, default="tgroup_hcount", choices=list(MODES.keys()), help="processing mode")
-  args = parser.parse_args()
+    metrics = [
+        "makespan",
+        "norm_makespan",
+        "norm_exp_makespan"
+    ]
 
-  with open(args.input_file) as input_file:
-    results = json.load(input_file)
+    groups = {
+        "app": {"label": "Application", "cond": get_app_group},
+        "hcount": {"label": "Hosts", "cond": get_host_count},
+        "fat": {"label": "Fat", "cond": get_fat},
+        "ccr": {"label": "CCR", "cond": get_ccr}
+    }
 
-  MODES.get(args.mode)(results)
+    output_modes = ["plain", "latex"]
+
+    parser = argparse.ArgumentParser(description="Experiment results analysis")
+    parser.add_argument("input_file", type=str, help="experiment results")
+    parser.add_argument("-a", "--algorithms", type=str, default=",".join(algorithms), help="algorithms")
+    parser.add_argument("-m", "--metric", type=str, default="norm_makespan", choices=list(metrics), help="metric")
+    parser.add_argument("-g1", "--group1", type=str, default="app", choices=list(groups), help="group 1")
+    parser.add_argument("-g2", "--group2", type=str, default="hcount", choices=list(groups), help="group 2")
+    parser.add_argument("-o", "--output", type=str, default="plain", choices=list(output_modes), help="output mode")
+    parser.add_argument("--std", action='store_true', help="print standard deviation")
+
+    args = parser.parse_args()
+    algorithms = args.algorithms.split(',')
+    group1 = groups.get(args.group1)
+    group2 = groups.get(args.group2)
+
+    with open(args.input_file) as input_file:
+        results = json.load(input_file)
+
+    compute_metric(results, args.metric)
+    grouped = group_results(results, algorithms, group1.get("cond"), group2.get("cond"))
+
+    if args.output == 'plain':
+        output_plain(grouped, algorithms, group1.get("label"), group2.get("label"), args.std)
+    elif args.output == 'latex':
+        output_latex(grouped, algorithms, group1.get("label"), group2.get("label"), args.std)
 
 
-def normtime_all_algo(results, cond1, cond2):
-  ALGO_ORDER = ["OLB", "MCT", "Random", "RoundRobin", "HCPT", "HEFT", "Lookahead", "PEFT"]
-  REFERENCE_ALGO = "OLB"
-  # evaluate normalized results
-  for task, bytask in groupby(results, get_taskfile_name):
-    for platform, byplat in groupby(bytask, get_platform_name):
-      algorithm_results = groupby(byplat, get_algorithm, False)
-      assert len(algorithm_results[REFERENCE_ALGO]) == 1
-      reference = algorithm_results[REFERENCE_ALGO][0]
-      for algorithm, byalg in algorithm_results.items():
-        byalg[0]["result"] = byalg[0]["makespan"] / reference["makespan"]
+def compute_metric(results, metric):
+    for task, bytask in groupby(results, get_app_filename):
+        for platform, byplat in groupby(bytask, get_platform_name):
+            algorithm_results = groupby(byplat, get_algorithm, False)
+            for algorithm, byalg in algorithm_results.items():
+                if metric == "makespan":
+                    byalg[0]["result"] = byalg[0]["makespan"]
+                if metric == "norm_makespan":
+                    reference_algo = "OLB"
+                    assert len(algorithm_results[reference_algo]) == 1
+                    reference = algorithm_results[reference_algo][0]
+                    byalg[0]["result"] = byalg[0]["makespan"] / reference["makespan"]
+                elif metric == "norm_exp_makespan":
+                    byalg[0]["result"] = byalg[0]["expected_makespan"] / byalg[0]["makespan"]
 
-  latex_table(results, ALGO_ORDER, cond1, cond2)
+
+def group_results(results, algorithms, cond1, cond2):
+    grouped_results = []
+    for c1, bycond1 in sorted(groupby(results, cond1)):
+        res1 = {'group': c1, 'results': []}
+        for c2, bycond2 in sorted(groupby(bycond1, cond2)):
+            res2 = {'group': c2, 'results': []}
+            for algorithm, byalg in sorted(groupby(bycond2, get_algorithm), key=lambda pair: algorithms.index(pair[0])):
+                mean = numpy.mean([r["result"] for r in byalg])
+                std = numpy.std([r["result"] for r in byalg])
+                res2['results'].append({'mean': mean, 'std': std})
+            res1['results'].append(res2)
+        grouped_results.append(res1)
+    return grouped_results
 
 
-def etime_static_algo(results, cond1, cond2):
-  ALGO_ORDER = ["HCPT", "HEFT", "Lookahead", "PEFT"]
-  REFERENCE_ALGO = "HEFT"
+def output_plain(results, algorithms, label1, label2, std):
+    print("".ljust(16), end="")
+    for alg in algorithms:
+        if std is True:
+            print(alg.ljust(15), end="")
+        else:
+            print(alg[:6].ljust(8), end="")
+    print("")
+    for group1_res in results:
+        print("%s=%s" % (label1, str(group1_res['group'])))
+        for group2_res in group1_res['results']:
+            print(("  %s=%s" % (label2, str(group2_res['group']))).ljust(16), end="")
+            for res in group2_res['results']:
+                if std is True:
+                    print(("%5.3f (%5.3f)" % (res['mean'], res['std'])).ljust(15), end="")
+                else:
+                    print(("%5.3f" % res['mean']).ljust(8), end="")
+            print("")
 
-  results = list(filter(lambda r: get_algorithm(r) in ALGO_ORDER, results))
-  # evaluate normalized results
-  for task, bytask in groupby(results, get_taskfile_name):
-    for platform, byplat in groupby(bytask, get_platform_name):
-      algorithm_results = groupby(byplat, get_algorithm, False)
-      assert len(algorithm_results[REFERENCE_ALGO]) == 1
-      reference = algorithm_results[REFERENCE_ALGO][0]
-      for algorithm, byalg in algorithm_results.items():
-        byalg[0]["result"] = byalg[0]["expected_makespan"] / reference["expected_makespan"]
 
-  latex_table(results, ALGO_ORDER, cond1, cond2)
+def par(string):
+    return textwrap.dedent(string).strip()
 
-def latex_table(results, algorithms, cond1, cond2):
-  # print results as latex table
-  # a lot of hardcode there for now. not sure if can be avoided without excessively generic code.
-  print(par(r"""
+
+def output_latex(results, algorithms, label1, label2, std):
+    print(par(r"""
   \begin{table}
   \caption{TODO}
   \begin{center}
      \small\begin{tabular}{*{%d}{l}}
   \toprule
-  GROUP 2 TODO & %s \\ \midrule
-  """) % (len(algorithms) + 1, " &  ".join(algorithms),))
-  for c1, bycond1 in sorted(groupby(results, cond1)):
+  %s & %s \\ \midrule
+  """) % (len(algorithms) + 1, label2, " &  ".join(algorithms),))
+    for group1_res in results:
+        print(par(r"""
+    \multicolumn{%d}{l}{%s=%s} \\ \midrule
+    """) % (len(algorithms) + 1, label1, str(group1_res['group'])))
+        for group2_res in group1_res['results']:
+            print("%-15s" % str(group2_res['group']), end="")
+            for res in group2_res['results']:
+                if std is True:
+                    print(" & %5.3f (%5.3f)" % (res['mean'], res['std']), end="")
+                else:
+                    print(" & %5.3f" % res['mean'], end="")
+            print(r" \\")
+        print(r"\midrule")
     print(par(r"""
-    \multicolumn{%d}{l}{GROUP 1 TODO %s} \\ \midrule
-    """) % (len(algorithms) + 1, c1))
-    for c2, bycond2 in sorted(groupby(bycond1, cond2)):
-      print("%-15s" % c2, end="")
-      for algorithm, byalg in sorted(groupby(bycond2, get_algorithm), key=lambda pair: algorithms.index(pair[0])):
-        mean = numpy.mean([r["result"] for r in byalg])
-        std = numpy.std([r["result"] for r in byalg])
-        print(" & %6.4f (%6.4f)" % (mean, std), end="")
-      print(r" \\")
-    print(r"\midrule")
-  print(par(r"""
   \bottomrule
   \end{tabular}
   \end{center}
@@ -136,5 +179,6 @@ def latex_table(results, algorithms, cond1, cond2):
   \end{table}
   """))
 
+
 if __name__ == "__main__":
-  main()
+    main()
--- dag/prepare.sh
+++ dag/prepare.sh
@@ -1,81 +1,102 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-DAGGEN_PATH="$HOME/devel/daggen/daggen"
-
 pushd "$SCRIPT_DIR/.."
 
+if [ -z "$DAGGEN_PATH" ]; then
+  echo "Specify path to daggen executable in DAGGEN_PATH environment variable"
+  exit 1
+fi
 if [ ! -f "$DAGGEN_PATH" ]; then
   echo "daggen executable $DAGGEN_PATH not found"
   exit 1
 fi
 
+SEED=1234
+
+# EXPERIMENT 1 (REAL WORKFLOWS) ****************************************************************************************
+
 #
-# Experiment 1:
+# Real workflows (CyberShake, Epigenomics, LIGO Inspiral, Montage)
+#
+# Tasks: 100
+# See tasks_exp1 directory
+#
+
+#
 # Heterogeneous clusters with homogeneous network a-la 1Gbit Ethernet.
 #
+# Nodes:     5, 10, 20
 # Speed:     1-4 GFLOPS
 # Bandwidth: 100 MBps
 # Latency:   100 us
 #
-if [ ! -d "$SCRIPT_DIR/plat_exp1" ]; then
-  python3 -m pysimgrid.tools.plat_gen "$SCRIPT_DIR/plat_exp1" 100 cluster 5 1-4 100 100 --include_master
-  python3 -m pysimgrid.tools.plat_gen "$SCRIPT_DIR/plat_exp1" 100 cluster 10 1-4 100 100 --include_master
-  python3 -m pysimgrid.tools.plat_gen "$SCRIPT_DIR/plat_exp1" 100 cluster 20 1-4 100 100 --include_master
+if [ ! -d "$SCRIPT_DIR/exp1_systems" ]; then
+  python3 -m pysimgrid.tools.plat_gen "$SCRIPT_DIR/exp1_systems" 100 $SEED cluster 5 1-4 100 100 --include_master
+  python3 -m pysimgrid.tools.plat_gen "$SCRIPT_DIR/exp1_systems" 100 $SEED cluster 10 1-4 100 100 --include_master
+  python3 -m pysimgrid.tools.plat_gen "$SCRIPT_DIR/exp1_systems" 100 $SEED cluster 20 1-4 100 100 --include_master
 fi
 
 #
-# Experiment 1_inf:
-# Heterogeneous clusters with homogeneous network a-la 1Gbit Ethernet.
+# Heterogeneous clusters with infinitely fast network (including loopback).
 #
+# Nodes:     5, 10, 20
 # Speed:     1-4 GFLOPS
 # Bandwidth: 1e12 MBps
-# Latency:   0
+# Latency:   0 us
 #
-if [ ! -d "$SCRIPT_DIR/plat_exp1_inf" ]; then
-  python3 -m pysimgrid.tools.plat_gen "$SCRIPT_DIR/plat_exp1_inf" 100 cluster 5 1-4 1e12 0 --include_master
-  python3 -m pysimgrid.tools.plat_gen "$SCRIPT_DIR/plat_exp1_inf" 100 cluster 10 1-4 1e12 0 --include_master
-  python3 -m pysimgrid.tools.plat_gen "$SCRIPT_DIR/plat_exp1_inf" 100 cluster 20 1-4 1e12 0 --include_master
+if [ ! -d "$SCRIPT_DIR/exp1_systems_inf" ]; then
+  python3 -m pysimgrid.tools.plat_gen "$SCRIPT_DIR/exp1_systems_inf" 100 $SEED cluster 5 1-4 1e12 0 \
+        --loopback_bandwidth=1e12 --loopback_latency=0 --include_master
+  python3 -m pysimgrid.tools.plat_gen "$SCRIPT_DIR/exp1_systems_inf" 100 $SEED cluster 10 1-4 1e12 0 \
+        --loopback_bandwidth=1e12 --loopback_latency=0 --include_master
+  python3 -m pysimgrid.tools.plat_gen "$SCRIPT_DIR/exp1_systems_inf" 100 $SEED cluster 20 1-4 1e12 0 \
+        --loopback_bandwidth=1e12 --loopback_latency=0 --include_master
 fi
 
-
-if [ ! -d "$SCRIPT_DIR/plat_exp2" ]; then
-  python3 -m pysimgrid.tools.plat_gen "$SCRIPT_DIR/plat_exp2" 1 cluster 10 1-4 100 100 --include_master
-fi
-
-
-if [ ! -d "$SCRIPT_DIR/plat_exp2_inf" ]; then
-  python3 -m pysimgrid.tools.plat_gen "$SCRIPT_DIR/plat_exp2_inf" 1 cluster 10 1-4 1e12 0 --loopback_bandwidth=1e12 --loopback_latency=0 --include_master
-fi
-
-
-if [ ! -d "$SCRIPT_DIR/tasks_exp2" ]; then
-  python3 -m pysimgrid.tools.dag_gen "$DAGGEN_PATH" "$SCRIPT_DIR/tasks_exp2" --count 100 --fat 0.5 0.65 0.8 --density 0.1 --jump 2 --force_ccr 1 10 100 500 1000 --repeat 100
-fi
+# EXPERIMENT 2 (SYNTHETIC WORKFLOWS) ***********************************************************************************
 
 #
-# Experiment 3:
-# Heterogeneous clusters with homogeneous network of different speeds.
+# Synthetic workflows
 #
+# Tasks:     100
+# Fat:       0.5, 0.65, 0.8 (10, 20, 40 tasks per layer)
+# Density:   0.1
+# Jump:      2
+# Data:      1-1000 MB
+# CCR:       1, 10, 100 MB/Gflop
+#
+if [ ! -d "$SCRIPT_DIR/exp2_workflows" ]; then
+  python3 -m pysimgrid.tools.dag_gen \
+        -n 100 --fat 0.5 0.65 0.8 --density 0.1 --jump 2 \
+        --mindata 1e6 --maxdata 1e9 --ccr 1 10 100 \
+        --repeat 100 --seed $SEED \
+        "$SCRIPT_DIR/exp2_workflows"
+fi
+
+#
+# Heterogeneous cluster with homogeneous network a-la 1Gbit Ethernet.
+#
+# Nodes:     10
 # Speed:     1-4 GFLOPS
 # Bandwidth: 100 MBps
 # Latency:   100 us
 #
-if [ ! -d "$SCRIPT_DIR/plat_exp3" ]; then
-  python3 -m pysimgrid.tools.plat_gen "$SCRIPT_DIR/plat_exp3" 1 cluster 5 1-4 100 100 --include_master
-  python3 -m pysimgrid.tools.plat_gen "$SCRIPT_DIR/plat_exp3" 1 cluster 10 1-4 100 100 --include_master
-  python3 -m pysimgrid.tools.plat_gen "$SCRIPT_DIR/plat_exp3" 1 cluster 20 1-4 100 100 --include_master
-  python3 -m pysimgrid.tools.plat_gen "$SCRIPT_DIR/plat_exp3" 1 cluster 5 1-4 20 100 --include_master
-  python3 -m pysimgrid.tools.plat_gen "$SCRIPT_DIR/plat_exp3" 1 cluster 10 1-4 20 100 --include_master
-  python3 -m pysimgrid.tools.plat_gen "$SCRIPT_DIR/plat_exp3" 1 cluster 20 1-4 20 100 --include_master
-  python3 -m pysimgrid.tools.plat_gen "$SCRIPT_DIR/plat_exp3" 1 cluster 5 1-4 10 100 --include_master
-  python3 -m pysimgrid.tools.plat_gen "$SCRIPT_DIR/plat_exp3" 1 cluster 10 1-4 10 100 --include_master
-  python3 -m pysimgrid.tools.plat_gen "$SCRIPT_DIR/plat_exp3" 1 cluster 20 1-4 10 100 --include_master
+if [ ! -d "$SCRIPT_DIR/exp2_systems" ]; then
+  python3 -m pysimgrid.tools.plat_gen "$SCRIPT_DIR/exp2_systems" 1 $SEED cluster 10 1-4 100 100 --include_master
 fi
 
-if [ ! -d "$SCRIPT_DIR/tasks_exp3" ]; then
-  python3 -m pysimgrid.tools.dag_gen "$DAGGEN_PATH" "$SCRIPT_DIR/tasks_exp3" --count 20 40 60 80 100 --fat 0.1 0.2 0.4 --density 0.1 0.2 0.3 0.4 --jump 1 2 3 --regular 0.1 0.5 0.9 --repeat 5
+#
+# Heterogeneous cluster with infinitely fast network (including loopback).
+#
+# Nodes:     10
+# Speed:     1-4 GFLOPS
+# Bandwidth: 1e12 MBps
+# Latency:   0 us
+#
+if [ ! -d "$SCRIPT_DIR/exp2_systems_inf" ]; then
+  python3 -m pysimgrid.tools.plat_gen "$SCRIPT_DIR/exp2_systems_inf" 1 $SEED cluster 10 1-4 1e12 0 \
+        --loopback_bandwidth=1e12 --loopback_latency=0 --include_master
 fi
-
 
 popd
